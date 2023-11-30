407a587c929eb0ad84c3a143fe67e41d048f3e87
uls19 <uls19@mail.ru>
2023-11-30 06:03:05 +0200

    header-select-blocks

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-const burgerPl = document.querySelector('.header__nav-burger');
-// const burgerOl = document.querySelector('.line__out');
-const menuNav = document.querySelector('.header__menu');
-burgerPl.addEventListener("click", function() {
-menuNav.classList.toggle('menu__show')
-});
-// burgerOl.addEventListener("click", function() {
-// menuNav.classList.toggle('menu__show');
-// });
+
+const burgerEl = document.querySelector('.header__nav-burger');
+  const menuEl = document.querySelector('.header__menu');
+ 
+  // вызываем элемент и скрываем если клик был за его пределами
+
+    const toggleMenu = function () {
+    menuEl.classList.toggle("menu__show");
+
+  }
+
+  burgerEl.addEventListener("click", function (e) {
+    e.stopPropagation();
+    toggleMenu();
+  });
+
+  document.addEventListener("click", function (e) {
+    const target = e.target;
+    const its_menu = target == menuEl || menuEl.contains(target);
+    const its_btnMenu = target == burgerEl;
+    const menu_is_active = menuEl.classList.contains("menu__show");
+
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+      toggleMenu();
+    }
+  });
