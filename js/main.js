41108f498a7915ec783b21e47c08264dd33848aa
uls19 <uls19@mail.ru>
2023-12-01 20:04:21 +0200

    po-pup-commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 
-const burgerEl = document.querySelector('.header__nav-burger');
+  const burgerEl = document.querySelector('.header__nav-burger');
   const menuEl = document.querySelector('.header__menu');
- 
+
+
   // вызываем элемент и скрываем если клик был за его пределами
 
-    const toggleMenu = function () {
+  const toggleMenu = function () {
     menuEl.classList.toggle("menu__show");
 
   }
@@ -23,4 +24,38 @@ const burgerEl = document.querySelector('.header__nav-burger');
     if (!its_menu && !its_btnMenu && menu_is_active) {
       toggleMenu();
     }
+  });
+
+  //po-pup
+  // const burgerEl = document.querySelector('.header__nav-burger');
+  const popupOut = document.querySelector('.pop-up__button-out');
+  const popupEl = document.querySelector('.pop-up');
+
+  // вызываем элемент и скрываем если клик был за его пределами
+
+  const togglePopup = function () {
+    popupEl.classList.toggle("popup__show");
+
+  }
+
+  burgerEl.addEventListener("click", function (e) {
+    e.stopPropagation();
+    togglePopup();
+  });
+
+  document.addEventListener("click", function (e) {
+    const target = e.target;
+    const its_popup = target == popupEl || popupEl.contains(target);
+    const its_btnPopup = target == burgerEl;
+    const popup_is_active = popupEl.classList.contains("popup__show");
+
+    if (!its_popup && !its_btnPopup && popup_is_active) {
+      togglePopup();
+    }
+  });
+
+  //скрываем элемент кнопкой
+
+  popupOut.addEventListener("click", function () {
+    popupEl.classList.toggle("popup__show");
   });
