f79434f2c3b9c51e1816df73efbe512d72635306
uls19 <uls19@mail.ru>
2023-12-03 00:38:06 +0200

    last-minute-slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-
+// //menu
   const burgerEl = document.querySelector('.header__nav-burger');
   const menuEl = document.querySelector('.header__menu');
-
-
   // вызываем элемент и скрываем если клик был за его пределами
 
   const toggleMenu = function () {
     menuEl.classList.toggle("menu__show");
-
   }
 
   burgerEl.addEventListener("click", function (e) {
@@ -20,42 +17,85 @@
     const its_menu = target == menuEl || menuEl.contains(target);
     const its_btnMenu = target == burgerEl;
     const menu_is_active = menuEl.classList.contains("menu__show");
-
+   
     if (!its_menu && !its_btnMenu && menu_is_active) {
       toggleMenu();
     }
   });
 
-  //po-pup
-  // const burgerEl = document.querySelector('.header__nav-burger');
-  const popupOut = document.querySelector('.pop-up__button-out');
-  const popupEl = document.querySelector('.pop-up');
+  // //po-pup
+  const popOut = document.querySelector('.pop-up__button-out');
+  const popEl = document.querySelector('.pop-up');
 
-  // вызываем элемент и скрываем если клик был за его пределами
+  // // вызываем элемент и скрываем если клик был за его пределами
 
-  const togglePopup = function () {
-    popupEl.classList.toggle("popup__show");
+  const togglePop = function () {
+    popEl.classList.toggle("popup__show");
 
   }
 
   burgerEl.addEventListener("click", function (e) {
     e.stopPropagation();
-    togglePopup();
+    togglePop();
   });
 
   document.addEventListener("click", function (e) {
     const target = e.target;
-    const its_popup = target == popupEl || popupEl.contains(target);
-    const its_btnPopup = target == burgerEl;
-    const popup_is_active = popupEl.classList.contains("popup__show");
+    const its_pop = target == popEl || popEl.contains(target);
+    const its_btnPop = target == burgerEl;
+    const pop_is_active = popEl.classList.contains("popup__show");
 
-    if (!its_popup && !its_btnPopup && popup_is_active) {
-      togglePopup();
+    if (!its_pop && !its_btnPop && pop_is_active) {
+      togglePop();
     }
   });
 
   //скрываем элемент кнопкой
 
-  popupOut.addEventListener("click", function () {
-    popupEl.classList.toggle("popup__show");
+  popOut.addEventListener("click", function () {
+    popEl.classList.toggle("popup__show");
+  });
+
+  
+//кнопки слайдера
+
+  const swiperEl = document.querySelector(".swiper");
+  const buttonNt = document.querySelector(".swiper-button-next");
+  const buttonPv = document.querySelector(".swiper-button-prev");
+
+  swiperEl.onmouseover = function (e) { 
+  buttonNt.style.opacity = '1';
+  buttonPv.style.opacity = '1';
+  }
+
+  buttonNt.onmouseover = function (e) {
+    buttonNt.style.opacity = '1';
+    buttonPv.style.opacity = '1';
+  }
+
+  buttonPv.onmouseover = function (e) {
+    buttonNt.style.opacity = '1';
+    buttonPv.style.opacity = '1';
+  }
+
+  swiperEl.onmouseout = function (e) { 
+        buttonNt.style.opacity = '0';
+        buttonPv.style.opacity = '0';
+      }
+
+  //слайдер код
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
   });
