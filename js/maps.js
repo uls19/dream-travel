9f3d713dd46bd222df71403a108c1747ca18d099
uls19 <uls19@mail.ru>
2024-01-19 01:55:04 +0200

    Maps-block-start

---
+++ js/maps.js
@@ -0,0 +1,15 @@
+//загрузка
+const mask = document.querySelector('.mask');
+window.addEventListener('load', () => {
+  mask.classList.add('hide');
+  setTimeout(() => {
+mask.remove();
+  }, 600);
+  });
+
+  const resetBtm = document.querySelector('.maps__top-button');
+  const formMaps = document.querySelector('.maps__form');
+  resetBtm.addEventListener('click', (e) => {
+    e.preventDefault(); // добавили, чтобы не перезагружалась страница
+    formMaps.reset();
+  });
